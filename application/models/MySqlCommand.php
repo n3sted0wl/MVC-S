ed957f0cf364a6f13b2d10118dae316f4b1c084d
Paul Antonio <paul.antonio2017@gmail.com>
2018-04-25 10:14:50 -0500

    Added description property to MySqlCommand

--- application/models/MySqlCommand.php
+++ application/models/MySqlCommand.php
@@ -2,9 +2,11 @@
     /** A Sql command to be executed against a MySql database */
     class MySqlCommand {
         private $_sqlCommand;
+        private $_description;
 
-        public function __construct(string $command) {
+        public function __construct(string $command, string $description) {
             $this->SetCommand($command);
+            $this->SetDescrpiption($description);
         }
 
         /** Get the SQL command as a string */
@@ -15,6 +17,16 @@
         /** Set the SQL command string */
         public function SetCommand(string $command) {
             $this->_sqlCommand = $command;
+        }
+
+        /** Get the description of the query */
+        public function GetDescription() {
+             return $this->_description;
+        }
+
+        /** Set the description of the query */
+        public function SetDescription(string $description) {
+            $this->_description = $description;
         }
     }
 ?>
