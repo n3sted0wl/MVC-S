44458226a43a1e64fb805f2ec583feb0e01d50da
Paul Antonio <paul.antonio2017@gmail.com>
2018-04-27 15:36:29 -0500

    Hotfix - Error in ajax request handler

--- static/js/site.js
+++ static/js/site.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
 /* ===================== Function definitions ===================== */
 /** A wrapper for making a jQuery ajax POST request; 
  * The only required option setting is "serviceName" */
-var sAjax = function(requestOptions) {
+var PerformService = function(requestOptions) {
     if (!requestOptions.hasOwnProperty("serviceName")) {
         console.log("Please set the 'serviceName' option");
     } else {
         // Override options that have to be the same for each request
         requestOptions.method   = "POST";
-        requestOptions.url      = "/services?serviceName=" + serviceName;
+        requestOptions.url      = "/services?serviceName=" + requestOptions.serviceName;
         requestOptions.dataType = "json";
         // Call the service
         $.ajax(requestOptions);
